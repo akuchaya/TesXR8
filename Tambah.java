de7d528e59a5f3d5a4e77e979a90e999445bd517
akuchaya <chaya_keyza_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 14:59:38 +0700

    Add files via upload

---
+++ Tambah.java
@@ -0,0 +1,17 @@
+
+
+public class Tambah {
+
+public static void main(String[] args) {
+
+    int a=1;
+    int b=200;
+    int hasil= a+b;
+
+    System.out.println("hasil dari" + a + " ditambah " +b+ " adalah " +hasil)
+    
+    
+    
+    ;
+}
+}
