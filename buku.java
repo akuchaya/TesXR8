ade9c8ea9daf8ea94775a146040970a9ee7fe547
akuchaya <chaya_keyza_33rpl@student.smktelkom-mlg.sch.id>
2024-08-10 22:15:12 +0700

    Add files via upload

---
+++ buku.java
@@ -0,0 +1,21 @@
+public class buku {
+    int id;
+    String judul;
+    String Pengarang;
+    int Tahunterbit;
+
+    public buku() {
+        id = 333;
+        judul ="monji";
+        Pengarang ="chaya";
+        Tahunterbit = 2020;
+} 
+//Constructor Parameter
+public buku (int id , String judul , String Pengarang , int Tahunterbit) {
+
+    this.id = id;
+    this.judul = judul;
+    this.Pengarang = Pengarang;
+    this.Tahunterbit = Tahunterbit;
+}}
+
