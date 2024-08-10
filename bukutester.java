cac74833d216f79462c24653db66fe0f593ca657
akuchaya <chaya_keyza_33rpl@student.smktelkom-mlg.sch.id>
2024-08-10 22:14:34 +0700

    Add files via upload

---
+++ bukutester.java
@@ -0,0 +1,59 @@
+public class bukutester {
+    
+        public static void main(String[] args) {
+           //object
+           buku monji = new buku();
+           buku dilan = new buku();
+           buku momo = new buku();
+           buku mome = new buku();
+           buku momi = new buku();
+
+           monji.id =333;
+           monji.judul= "monji";
+           monji.Pengarang = "chaya";
+           monji.Tahunterbit =2020;
+
+           dilan.id =444;
+           dilan.judul= "dilan";
+           dilan.Pengarang = "chayalagi";
+           dilan.Tahunterbit =2018;
+
+           momo.id =33333;
+           momo.judul= "momo";
+           momo.Pengarang = "chayajuga";
+           momo.Tahunterbit =2021;
+
+           mome.id =333783;
+           mome.judul= "mome";
+           mome.Pengarang = "chayaterus";
+           mome.Tahunterbit =2017;
+
+           momi.id =3303933;
+           momi.judul= "momi";
+           momi.Pengarang = "chayalagiii";
+           momi.Tahunterbit =2014;
+    
+           System.out.println(monji.id);
+           System.out.println(monji.judul);
+           System.out.println(monji.Pengarang);
+           System.out.println(monji.Tahunterbit);
+           System.out.println(dilan.id);
+           System.out.println(dilan.judul);
+           System.out.println(dilan.Pengarang);
+           System.out.println(dilan.Tahunterbit);
+           System.out.println(momo.id);
+           System.out.println(momo.judul);
+           System.out.println(momo.Pengarang);
+           System.out.println(momo.Tahunterbit);
+          
+           System.out.println(mome.id);
+           System.out.println(mome.judul);
+           System.out.println(mome.Pengarang);
+           System.out.println(mome.Tahunterbit);
+          
+           System.out.println(momi.id);
+           System.out.println(momi.judul);
+           System.out.println(momi.Pengarang);
+           System.out.println(momi.Tahunterbit);
+          
+} }
