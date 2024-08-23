327bb0a62267c62196a726befda6b845da829fb0
akuchaya <chaya_keyza_33rpl@student.smktelkom-mlg.sch.id>
2024-08-23 10:36:11 +0700

    Add files via upload

---
+++ diskon.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+public class diskon {
+    public static void main (String[] args) throws InterruptedException{
+
+        // start
+
+        System.out.println("Kalkulator Diskon");
+        System.out.println("===========================================================================================");
+
+        // input harga
+      
+        System.out.println("Masukkan harga awal barang. (Angka saja)");
+        double harga = new Scanner(System.in).nextDouble();
+        System.out.println("===========================================================================================");
+
+        // input diskon
+
+        System.out.println("Masukkan diskon barang. (Angka saja)");
+        double diskon = new Scanner(System.in).nextDouble();
+        diskon /= 100;
+        System.out.println("===========================================================================================");
+
+        // input jumlah
+
+        System.out.println("Masukkan jumlah barang yang dibeli. (Angka saja)");
+        int jumlah = new Scanner(System.in).nextInt();
+        System.out.println("===========================================================================================");
+
+        // equation
+
+        double totalHarga = (harga - (harga * diskon)) * jumlah;
+
+        // output
+
+        System.out.println("Mohon tunggu hasil dari nilai yang anda masukkan.");
+        TimeUnit.SECONDS.sleep(5);
+        System.out.println("Total harga dari belanjaan anda adalah Rp" + totalHarga + ".");
+        System.out.println("===========================================================================================");
+        System.out.println("Sekian, terimakasih.");
+        System.out.println("Code written by @chayarkd.");
+
+    }
+
+}
