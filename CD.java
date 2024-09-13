f201ce4646ef9bbfe220eafb620e18acd4ff520d
akuchaya <chaya_keyza_33rpl@student.smktelkom-mlg.sch.id>
2024-09-13 14:27:58 +0700

    Add files via upload

---
+++ CD.java
@@ -0,0 +1,45 @@
+package Inventory;
+
+public class CD extends Product {
+
+    String artist;
+    int numSong;
+    String label;
+    
+
+    public String getArtist() {
+    return this.artist;
+    
+    }
+    public void setArtist(String artist) {
+    this.artist = artist;
+    }
+    public int getNumSong() {
+    return this.numSong;
+    }
+    public void setNumSong(int numSong) {
+    this.numSong = numSong;
+
+    }
+
+    public String getLabel() {
+    return this.label;
+    }
+    public void setLabel(String label) {
+    this.label = label;
+    }
+    public CD() {
+    super();
+    artist="";
+    numSong=0;
+    label="";
+    }
+    public CD(String artist, int numSong, String label, String name, int number, int quantity, double price) {
+    super(name, number, quantity, price);
+    this.artist=artist;
+    this.numSong=numSong;
+    this.label=label;
+
+    }
+    
+    }
